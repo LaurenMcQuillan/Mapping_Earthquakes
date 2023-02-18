20c32ba13b822b65524d4ac6ecd74bc738fcacd3
LaurenMcQuillan <115508658+LaurenMcQuillan@users.noreply.github.com>
2023-02-17 20:07:39 -0500

    Challenge Submission Revised Colors

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -137,12 +137,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 6) {
-      return "red";
+      return "#ea2c2c";
     }
     if (magnitude > 5) {
-      return "orange";
+      return "#ea822c";
     }
-    return "yellow";
+    return "#ee9c00";
   }
   
   // 6. Use the function that determines the radius of the earthquake marker based on its magnitude. // Might need to check later
---
+++ Earthquake_Challenge/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibGF1cmVuLW1jcXVpbGxhbi03ODkiLCJhIjoiY2xkYXdqMXo2MDZkbTN2cXJ0cnlqNHAxcSJ9.1YS6q_gurxxh3izd61zEjw";
